ebb266d74b4c1c63d060e3658739db478b4d55b4
PedroMaiorano <speorks@gmail.com>
2024-06-22 11:56:12 -0300

    sa

--- server.R
+++ server.R
@@ -1,3 +1,4 @@
+#.s
 
 movies = read.csv("./data/ml-1m/movies.dat", sep=":", colClasses = c(NA, "NULL"), header = F)
 names(movies) <- c('item_id','name','genres')
@@ -17,16 +18,46 @@ ratings <- as(ratings, "realRatingMatrix")
 train <- ratings
 rec <- Recommender(train, method = "UBCF")
 
+generos_unidos <- paste( unique(movies$genres), collapse = "|")
+generos_separados <- unlist(strsplit(generos_unidos, "\\|"))
+generos_unicos <- unique(generos_separados)
+generos_para_remover <- c("", " Miami Beach (1988)")
+generos_filtrados <- setdiff(generos_unicos, generos_para_remover)
+
+
 
 server <- function(input, output) {
 
     actual_page <- reactiveValues(data = 1)
-    filtered_movies <- reactiveValues(data = movies)
+    filtered_movies <- reactiveVal(NULL)
     n_rows <- reactiveValues(data = 3)
     n_movies_per_row <- reactiveValues(data = 4)
     recommender <- reactiveValues(object = rec)
-
-
+    
+    filtered_movies <- reactive({
+      Base <- NULL
+      if (gsub(" ", "", input$nome) == ""){
+        Base <- movies
+      }
+      else{
+        Base <- movies[grepl(input$nome, movies$name),]
+      }
+      
+      if (length(input$genre) > 0){
+        Base <- Base[grepl(input$genre, movies$genres), ]
+      }
+      #n_rows$data <- (nrow(Base) %% 12) %% 4
+      
+      actual_page$data <- 1
+      Base
+     
+    })
+    
+    
+    output$dataframe <- renderTable({
+      filtered_movies()
+    })
+    
 
 
     observeEvent(input$previous_page, {
@@ -39,7 +70,7 @@ server <- function(input, output) {
     observeEvent(input$next_page, {
         n_rows = n_rows$data
         n_movies_per_row = n_movies_per_row$data
-        n_pages = as.integer(nrow(filtered_movies$data)/(n_rows*n_movies_per_row))
+        n_pages = as.integer(nrow(filtered_movies())/(n_rows*n_movies_per_row))
         print(actual_page$data)
         print(n_pages)
         if (actual_page$data < n_pages){
@@ -48,7 +79,7 @@ server <- function(input, output) {
     })
 
     output$page <- renderText({ actual_page$data })
-
+    
     observeEvent(input$model, {
         if (input$model == "UBCF"){
             recommender$object <- Recommender(train, method = "UBCF")
@@ -64,7 +95,7 @@ server <- function(input, output) {
         item <- c()
         rating <- c()
 
-        for (i in filtered_movies$data$item_id){
+        for (i in filtered_movies()$data$item_id){
             title = movies[movies$item_id==i,][['name']]
             if (!is.null(input[[paste0("select_",i)]])){
                 print("val")
@@ -101,49 +132,139 @@ server <- function(input, output) {
         pre <- predict(recommender$object, ratings, n = 5)
         print(as(pre, "list"))
     })
+    
+    
 
+    ratings <- reactiveValues(data = list())
+    
     output$movies_grid <- renderUI({
-        n_rows = n_rows$data
-        n_movies_per_row = n_movies_per_row$data
-        
-
-        actual_page = actual_page$data
-
-        filtered_movies = filtered_movies$data
-
-        n_pages = as.integer(nrow(filtered_movies)/(n_rows*n_movies_per_row))
-
-        lapply(1:n_rows,
-            function(i) {
-                list(fluidRow(lapply(1:n_movies_per_row, function(j) {
-                    list(column(width = 3, height=250,
-                        div(
-                            style = "text-align:center; padding-bottom: 13px;",
-                            div(
-                                style = "text-align:center",
-                                img(src = filtered_movies$image[(i - 1) * n_movies_per_row + j + (actual_page-1)*(n_rows*n_movies_per_row)],height = 150)
-                            ),
-                            div(
-                                style = "text-align:center; max-height: 40px; height: 40px",
-                                strong(filtered_movies$name[(i - 1) * n_movies_per_row + j + (actual_page-1)*(n_rows*n_movies_per_row)])
-                            ),
-                            div(
-                                style = "text-align:center; font-size: 150%; color: #f0ad4e;",
-                                ratingInput(paste0("select_", filtered_movies$item_id[(i - 1) * n_movies_per_row + j + (actual_page-1)*(n_rows*n_movies_per_row)]), label = "", dataStop = 5)
-                            ),
-                            # div(
-                            #     paste0("select_", filtered_movies$item_id[(i - 1) * n_movies_per_row + j + (actual_page-1)*(n_rows*n_movies_per_row)])
-                            # )
-                        )
-                            
-                        )
-                    )
-                })))
+      n_rows <- n_rows$data
+      n_movies_per_row <- n_movies_per_row$data
+      actual_page <- actual_page$data
+      n_pages <- as.integer(nrow(filtered_movies()) / (n_rows * n_movies_per_row))
+      w <- nrow(filtered_movies())
+      
+      lapply(1:n_rows, function(i) {
+        list(fluidRow(lapply(1:n_movies_per_row, function(j) {
+          index <- (i - 1) * n_movies_per_row + j + (actual_page - 1) * (n_rows * n_movies_per_row)
+          if (index <= linhas_base) {
+            movie_id <- filtered_movies()$item_id[index]
+            rating_id <- paste0("select_", movie_id)
+            
+            # Se a classificação para esse filme ainda não existe, inicialize com NULL
+            if (is.null(ratings$data[[rating_id]])) {
+              ratings$data[[rating_id]] <- 0
             }
-        
-        )
-
-
+            
+            list(column(width = 3, height = 250,
+                        div(
+                          style = "text-align:center; padding-bottom: 13px;",
+                          div(
+                            style = "text-align:center",
+                            img(src = filtered_movies()$image[index], height = 150)
+                          ),
+                          div(
+                            style = "text-align:center; max-height: 40px; height: 40px",
+                            strong(filtered_movies()$name[index])
+                          ),
+                          div(
+                            style = "text-align:center; font-size: 150%; color: #f0ad4e;",
+                            ratingInput(
+                              inputId = rating_id,
+                              label = "",
+                              value = ratings$data[[rating_id]],
+                              dataStop = 5
+                            )
+                          )
+                        )
+            ))
+          } else {
+            list(column(width = 3, height = 250,
+                        div(
+                          style = "text-align:center; padding-bottom: 13px;",
+                          div(
+                            style = "text-align:center; height: 150px;"
+                          ),
+                          div(
+                            style = "text-align:center; max-height: 40px; height: 40px"
+                          ),
+                          div(
+                            style = "text-align:center; font-size: 150%; color: #f0ad4e;"
+                          )
+                        )
+            ))
+          }
+        })))
+      })
     })
+    
+    
+    observe({
+      lapply(names(ratings$data), function(rating_id) {
+        observeEvent(input[[rating_id]], {
+          ratings$data[[rating_id]] <- input[[rating_id]]
+        })
+      })
+    })
+    
+    #output$movies_grid_avaliados <- renderUI({
+    #  avaliados <- names(ratings$data)[sapply(ratings$data, function(x) x > 0)]
+    #  filmes_avaliados <- filtered_movies()[filtered_movies()$item_id %in% sub("select_", "", avaliados),]
+    #  
+    #  n_rows <- n_rows$data
+    #  n_movies_per_row <- n_movies_per_row$data
+    #  linhas_base <- nrow(filmes_avaliados)
+    #  
+    #  lapply(1:n_rows, function(i) {
+    #    list(fluidRow(lapply(1*n_movies_per_row, function(j) {
+    #      index <- (i - 1) * n_movies_per_row + j
+    #      if (index <= linhas_base) {
+    #        movie_id <- filmes_avaliados$item_id[index]
+    #        rating_id <- paste0("select_", movie_id)
+    #        
+    #        list(column(width = 3, height = 250,
+    #                    div(
+    #                      style = "text-align:center; padding-bottom: 13px;",
+    #                      div(
+    #                        style = "text-align:center",
+    #                        img(src = filmes_avaliados$image[index], height = 150)
+    #                      ),
+    #                      div(
+    #                        style = "text-align:center; max-height: 40px; height: 40px",
+    #                        strong(filmes_avaliados$name[index])
+    #                      ),
+    #                      div(
+    #                        style = "text-align:center; font-size: 150%; color: #f0ad4e;",
+    #                        ratingInput(
+    #                          inputId = rating_id,
+    #                          label = "",
+    #                          value = ratings$data[[rating_id]],
+    #                          dataStop = 5
+    #                        )
+    #                      )
+    #                    )
+    #        ))
+    #      } else {
+    #        list(column(width = 3, height = 250,
+    #                    div(
+    #                      style = "text-align:center; padding-bottom: 13px;",
+    #                      div(
+    #                        style = "text-align:center; height: 150px;"
+    #                      ),
+    #                      div(
+    #                        style = "text-align:center; max-height: 40px; height: 40px"
+    #                      ),
+    #                      div(
+    #                        style = "text-align:center; font-size: 150%; color: #f0ad4e;"
+    #                      )
+    #                    )
+    #        ))
+    #      }
+    #    })))
+    #  })
+    #})           
+            
+        
 
 }
+shinyApp(ui = ui, server = server)
--- ui.R
+++ ui.R
@@ -19,12 +19,12 @@ model_column <- box(width = NULL, status = "warning",
     actionButton("update_model", "Atualizar Modelo")
 )
 
-filter_column <- column(width = 3,
+filter_column <- column(width = 12,
     box(width = NULL, status = "warning",
         textInput("nome", "Nome do Filme", ""),
         selectInput(
             "genre", "Selecione o Genero do Filme:",
-            list("Action", "Drama", "Horror", "Romance"), multiple=TRUE
+            generos_filtrados, multiple=TRUE
         ),
         dateRangeInput("ano", "Ano de Estreia do Filme:",
             start = "1920-01-01", end = "2018-12-31", format = "yyyy",separator = " Até "
@@ -39,27 +39,47 @@ filter_column <- column(width = 3,
 
 
 
-movies_column <- column(width = 9,
-    box(width = NULL, solidHeader = TRUE,
-        uiOutput('movies_grid'),
-
-        fluidRow(
-            actionButton("previous_page", "Pagina Anterior"),
-            textOutput("page"),
-            actionButton("next_page", "Proxima Pagina")
-        )
-    ),
-    box(width = NULL,
-        actionButton("gen_recommendation", "Gerar Recomendação"),
-        uiOutput('recommendation')
-    )
-)
-
+movies_column <- box(width = NULL, solidHeader = TRUE,tabsetPanel(
+                  
+                    tabPanel(
+                titlePanel("Avaliação"),
+                column(width = 12,br(),
+                  
+                      uiOutput('movies_grid'),
+              
+                      fluidRow(
+                          actionButton("previous_page", "Pagina Anterior"),
+                          textOutput("page"),
+                          actionButton("next_page", "Proxima Pagina")
+                      )
+                  )),
+              #    box(width = NULL,
+              #        actionButton("gen_recommendation", "Gerar Recomendação"),
+              #        uiOutput('recommendation')
+              #    )
+              #),
+              tabPanel(titlePanel("Avaliados"),
+                       column(width = 12, br(),
+                              uiOutput('movies_grid_avaliados')
+                              )),
+              
+              tabPanel(titlePanel("Recomendação"),
+                           column(width = 12, br(),
+                                  tableOutput("dataframe")))))
+      
 body <- dashboardBody(
-  fluidRow(
-    filter_column,
-    movies_column
-  )
+#  dropdownButton(
+#    tags$h3("List of Input"),
+#    selectInput(inputId = 'xcol', label = 'X Variable', choices = names(iris)),
+#    selectInput(inputId = 'ycol', label = 'Y Variable', choices = names(iris), selected = names(iris)[[2]]),
+#    sliderInput(inputId = 'clusters', label = 'Cluster count', value = 3, min = 1, max = 9),
+#    circle = TRUE,
+#    status = "danger", 
+#    icon = icon("gear"), width = "300px",
+#    tooltip = tooltipOptions(title = "Click to see inputs !")
+#  ),
+  fluidRow(column(3,filter_column),
+           column(9,movies_column))
 )
 
 ui <- dashboardPage(
@@ -67,3 +87,4 @@ ui <- dashboardPage(
   dashboardSidebar(disable = TRUE),
   body
 )
+
